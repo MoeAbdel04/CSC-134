64f0066801c13ede250b69b31bf340e93ffeafa9
MoeAbdel04 <145929908+MoeAbdel04@users.noreply.github.com>
2024-01-24 11:11:54 -0500

    Add files via upload

---
+++ M1T1_Abdel.cpp
@@ -0,0 +1,26 @@
+/*
+CSC 134
+M1T2 - Hello World
+Mahamed Abdel
+1/17/2024
+*/
+#include <iostream>
+using namespace std;
+int main() {
+ // for now we'll introduce ourselves
+ //Mahamed Abdel
+ string name;
+ cout << "hello, I'm Mahamed Abdel" << name << endl;
+ // print your favorite pokemon or vehicle
+ cout << "My favorite vehicle is a Porche 911 GT2rs" << endl;
+ // ask the user a question
+ cout << "Whats yours?" << endl;
+ string mon; 
+ cin >> mon;
+ cout << "Oh, " << mon << endl;
+ cout << "That's cool." << endl;
+ 
+ 
+ 
+ return 0;
+}
