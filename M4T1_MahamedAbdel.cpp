7406cd0297861bc3b2702ef8a1b57f2a4fd7e930
MoeAbdel04 <145929908+MoeAbdel04@users.noreply.github.com>
2024-02-28 19:05:30 -0500

    Add files via upload

---
+++ M4T1_MahamedAbdel.cpp
@@ -0,0 +1,26 @@
+/*
+CSC-134
+M4T1 - While loops
+Mahmed Abdel
+02/28/2004
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    const int MAX = 9;
+    while (count < MAX)
+        cout << "counting: " << count << endl;
+
+        count += 1;
+
+
+}
+return 0;
+
+
+
